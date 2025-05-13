b39893a9515a8a167d37be0fd87d3ae46b1a1696
Laghrabi <laghrabichaimae@gmail.com>
2025-05-13 21:02:51 +0100

    AST structure

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: claghrab <claghrab@student.42.fr>          +#+  +:+       +#+        */
+/*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:10:39 by claghrab          #+#    #+#             */
-/*   Updated: 2025/05/07 15:31:35 by claghrab         ###   ########.fr       */
+/*   Updated: 2025/05/13 20:49:45 by claghrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-#include "libft/libft.h"
+# include "libft/libft.h"
+
+t_token	*g_token;
 
 /* ENUMERATION */
 typedef enum s_type {
@@ -39,11 +41,27 @@ typedef enum s_type {
     T_RPAREN
 } t_type;
 
+typedef enum e_node_type {
+    NODE_CMD,
+    NODE_PIPE,
+    NODE_AND,
+    NODE_OR,
+    NODE_REDIR,
+    NODE_SUBSHELL
+} t_node_type;
+
 /* STRUCTURES */
+typedef struct s_ast {
+    t_type          type;
+    t_token         *token_list;    // for NODE_CMD
+    struct s_ast    *left;
+    struct s_ast    *right;
+} t_ast;
+
 typedef struct s_token {
-    char    *value;
-    t_type  token;
-    struct s_token *next;
+    t_type          token;
+    char            *value;
+    struct s_token  *next;
 } t_token;
 
 typedef struct s_info {
@@ -55,6 +73,11 @@ typedef struct s_info {
 
 
 /* FUNCTIONS */
+int	is_red_list(char *str);
+t_token	*peek(void);
+t_token	*consume(void);
+void	*syntax_error(void);
+t_ast	*create_ast_node(t_ast *left, t_ast *right, t_token	*token_list, t_node_type type);
 t_token *tokenization(char *input);
 void word_sep(char *input, t_info info, int *i, t_token **token);
 void operator_sep(char *input, t_info info, int *i, t_token **token);
