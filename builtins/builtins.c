9e839d32d63804625f4dbdafeda41ebc759e4737
Laghrabi <laghrabichaimae@gmail.com>
2025-05-28 16:21:13 +0100

    implemented cd

--- builtins/builtins.c
+++ builtins/builtins.c
@@ -6,65 +6,11 @@
 /*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 14:16:51 by claghrab          #+#    #+#             */
-/*   Updated: 2025/05/27 14:20:22 by claghrab         ###   ########.fr       */
+/*   Updated: 2025/05/28 16:14:56 by claghrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-void	cmd_or_builtin(t_token *token)
-{
-	int flag;
-	
-	if (token == NULL)
-		return ;
-	expansion();
-	flag = if_builtin(token->value);
-	if (if_builtin(token->value) == 0)
-		exec_cmd();
-	else
-		which_one(flag, token);
-}
-
-void	which_one(int flag, t_token *token)
-{
-	if (flag == 1)
-		builtin_cd(token);
-	else if (flag == 2)
-		builtin_echo(token);
-	else if (flag == 3)
-		builtin_pwd(token);
-	else if (flag == 4)
-		builtin_export(token);
-	else if (flag == 5)
-		builtin_unset(token);
-	else if (flag == 6)
-		builtin_env(token);
-	else if (flag == 7)
-		builtin_exit(token);
-}
-
-int	if_builtin(char *cmd)
-{
-	if (cmd == NULL)
-		return (0);
-	if (ft_cmp(cmd, "cd") == 0)
-		return (1);
-	else if (ft_strcmp(cmd, "echo") == 0)
-		return (2);
-	else if (ft_strcmp(cmd, "pwd") == 0)
-		return (3);
-	else if (ft_strcmp(cmd, "export") ==0)
-		return (4);
-	else if (ft_strcmp(cmd, "unset") == 0)
-		return (5);
-	else if (ft_strcmp(cmd, "env") == 0)
-		return (6);
-	else if (ft_strcmp(cmd, "exit") == 0)
-		return (7);
-	else
-		return (0);
-}
 
 int	builtin_echo(t_token *token)
 {
@@ -93,6 +39,68 @@ int	builtin_echo(t_token *token)
 		token = token->next;
 	}
 	if (nl == 0 && write(1, "\n", 1) == -1)
+		return (1);
+	return (0);
+}
+
+int	cd_helper(char *new_path, t_env *env_list)
+{
+	char	*old_pwd;
+	char	*new_pwd;
+	
+	if (new_path == NULL || env_list == NULL)
+		return (1);
+	old_pwd = getcwd(NULL, 0);
+	if (chdir(new_path) == -1)
+	{
+		printf("cd: %s: No such file or directory\n", new_path);
+		free(old_pwd);
+		garbage_collector(NULL, 1);
+		return (1);
+	}
+	new_pwd = getcwd(NULL, 0);
+	update_env("OLDPWD", old_pwd, env_list);
+	update_env("PWD", new_pwd, env_list);
+	free(old_pwd);
+	free(new_pwd);
+	return (0);
+}
+
+int	builtin_cd(t_token *token, t_env *env_list)
+{
+	char	*new_path;
+	
+	if (token == NULL || env_list == NULL)
+		return (1);
+	if (token->next == NULL || ft_strcmp(token->next->value, "~") == 0)
+		new_path = get_env_value("HOME", env_list);
+	else if (ft_strcmp(token->next->value, "-") == 0)
+		new_path = get_env_value("OLDPWD", env_list);
+	else
+		new_path = token->next->value;
+	if (cd_helper(new_path, env_list) == 1)
+		return (1);
+	return (0);
+}
+
+int	builtin_pwd(t_token *token, t_env *env_list)
+{
+	char	*pwd;
+	
+	if (token == NULL || env_list == NULL)
+		return (1);
+	if (token->next != NULL)
+	{
+		printf("pwd: too many arguments\n");
+		return (1);
+	}
+	pwd = get_env_value("PWD", env_list);
+	if (pwd == NULL)
+	{
+		printf("pwd: PWD not set\n");
+		return (1);
+	}
+	if (printf("%s\n", pwd) == -1)
 		return (1);
 	return (0);
 }
