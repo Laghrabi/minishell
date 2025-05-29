f3144cdaf7a6eb89c7cbe6363cfcfe3ce49af10d
ziadhunter <farouk.ziad.2002@gmail.com>
2025-05-29 19:31:51 +0100

    expansion is done ,time for test now

--- parsing/expantion/expantion.c
+++ parsing/expantion/expantion.c
@@ -6,67 +6,66 @@
 /*   By: zfarouk <zfarouk@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 16:04:58 by zfarouk           #+#    #+#             */
-/*   Updated: 2025/05/28 16:25:22 by zfarouk          ###   ########.fr       */
+/*   Updated: 2025/05/29 19:26:26 by zfarouk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int wich_type(char c, int *db_quote, int *sg_quote)
+int which_type(char c, int *db_quote, int *sg_quote, int *quote_state)
 {
-    static char i = 0;
-
     if (c == '\'')
     {
         if (*db_quote == 1)
-            return (i);
+            return (quote_state);
         else if(*sg_quote == 1)
         {
             *sg_quote = 0;
-            i = 0;
+            quote_state = 0;
         }
         else if (*sg_quote == 0)
         {
             *sg_quote = 1;
-            i = 2;
+            quote_state = 2;
 			return (0);
         }
     }
     else if (c == '\"')
     {
         if (*sg_quote == 1)
-            return (i);
+            return (quote_state);
         if (*db_quote == 1)
         {
             *db_quote = 0;
-            i = 0;
+            quote_state = 0;
         }
         else if (*db_quote == 0)
         {
             *db_quote = 1;
-            i = 4;
+            quote_state = 4;
 			return (0);
         }
     }
-    return (i);
+    return (quote_state);
 }
 
 int *set_field(char *str)
 {
-    char *field;
+    int *field;
     int (i), (db_quote), (sg_quote);
     char type;
+    int quote_state;
     
     i = 0;
 	db_quote = 0;
 	sg_quote = 0;
-    field = gc_malloc((ft_strlen(str) + 1) * sizeof(int));
+    quote_state = 0;
+    field = gc_malloc((ft_strlen(str)) * sizeof(int));
     while (str[i])
     {
-		type = which_type(str[i], &db_quote, &sg_quote);
+		type = which_type(str[i], &db_quote, &sg_quote, &quote_state);
         field[i++] = type;
     }
-	field[i] = '\0';
 	return field;
 } 
 int extra_quote(char c, int i)
@@ -76,10 +75,86 @@ int extra_quote(char c, int i)
     return (0);
 }
 
-char *expanded_token(char *token, char *field, t_env *env)
+char *check_var(char *token, t_env *env)
+{
+    size_t i;
+    char *var_name;
+
+    i = 0;
+    if (token[0] == ' ' || token[i] == '\t' || token[i] == '\0' || token[i] == '\"' || token[i] == '\'')
+        return (ft_strdup("$"));
+    while (token[i] != ' ' && token[i] != '\t' && token[i] != '\0'&& token[i] != '\"' && token[i] != '\'')
+        i++;
+    var_name = ft_substr(token, 0, i);
+    return (get_env_value(var_name, env));
+}
+
+void skip_var(char *token, int *i)
+{
+    (*i)++;
+    while (token[*i] != ' ' && token[*i] != '\t' && token[*i] != '\0'&& token[*i] != '\"' && token[*i] != '\'')
+        *i++;
+    (*i)--;
+}
+
+void fix_value(char *value)
+{
+    int (i), (j);
+    int in_space;
+
+    i = 0;
+    j = 0;
+    in_space = 0;
+    while (value[i] == ' ' || value[i] == '\t')
+        i++;
+    while (value[i])
+    {
+        if (value[i] == ' ' || value[i] == '\t')
+        {
+            if (!in_space)
+                value[j++] = ' ';
+            in_space = 1;
+        }
+        else
+        {
+            value[j++] = value[i];
+            in_space = 0;
+        }
+        i++;
+    }
+    if (j > 0 && value[j - 1] == ' ')
+        j--;
+    value[j] = '\0';
+}
+
+int  write_value(char **token, char *value, int *field, db_int yes)
+{
+    int len;
+    int i;
+    char *new_token;
+    
+    if (field[yes.i] == 4)
+        fix_value(value);
+    len = ft_strlen(value);
+    new_token = gc_malloc((yes.len + len + 1) * sizeof(char));
+    len = 0;
+    while ((*token)[len])
+    {
+        new_token[len] = (*token)[len];
+        len++;
+    }
+    i = -1;
+    while (value[++i])
+        new_token[len + i] = value[i];
+    *token = new_token;
+    return (len + i);
+}
+
+char *expanded_token(char *token, int *field, t_env *env)
 {
     int (i), (j), (len), (quote);
     char *new_token;
+    char *value;
 
     len = ft_strlen(token);
     new_token = gc_malloc(len * sizeof(char) + 1);
@@ -89,13 +164,22 @@ char *expanded_token(char *token, char *field, t_env *env)
     {
         if (extra_quote(token[i], field[i]))
             continue;
-        if (token[i] == '$')
-            check_var
+        if (token[i] == '$' && field[i] != 2)
+        {
+            value = check_var(&token[i+1], env);
+            if (value)
+                j = write_value(&new_token, value, field, (db_int){i, len});
+            else
+                skip_var(token, &i);
+        }
+        else
+            new_token[j++] = token[i]; 
     }
-    
+    new_token[j] = '\0';
+    return (new_token);
 }
 
-void expantion(t_token *arg_list, t_env *env)
+void expansion(t_token *arg_list, t_env *env)
 {
     int *field;
 
