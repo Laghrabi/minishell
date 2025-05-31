3bfef4550dd79f959539be83e7e8485a5aa9f7d1
Laghrabi <laghrabichaimae@gmail.com>
2025-05-31 17:17:34 +0100

    implemented unset and env

--- builtins/builtins.c
+++ builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 14:16:51 by claghrab          #+#    #+#             */
-/*   Updated: 2025/05/28 16:14:56 by claghrab         ###   ########.fr       */
+/*   Updated: 2025/05/31 17:11:54 by claghrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,4 +103,119 @@ int	builtin_pwd(t_token *token, t_env *env_list)
 	if (printf("%s\n", pwd) == -1)
 		return (1);
 	return (0);
+}
+int	builtin_env(t_token *token, t_env *env_list)
+{
+	t_env *current;
+
+	if (token == NULL || env_list == NULL)
+		return (1);
+	if (token->next != NULL)
+	{
+		printf("env: too many arguments\n");
+		return (1);
+	}
+	current = env_list;
+	while (current != NULL)
+	{
+		if (current->value != NULL)
+			printf("%s=%s\n", current->key, current->value);
+		current = current->next;
+	}
+	return (0);
+}
+
+int	builtin_unset(t_token *token, t_env **env_list)
+{
+	t_env *current;
+
+	if (token == NULL || env_list == NULL  || *env_list == NULL)
+		return (1);
+	token = token->next;
+	while (token != NULL && ft_strcmp((*env_list)->key, token->value) == 0)
+	{
+		*env_list = (*env_list)->next;
+		token = token->next;	
+	}
+	while (token != NULL)
+	{
+		current	= *env_list;
+		while (current != NULL)
+		{
+			if (current->next != NULL && ft_strcmp(token->value, current->next->key) == 0)
+			{
+				current->next = current->next->next;
+				break ;
+			}
+			current = current->next;
+		}
+		token = token->next;
+	}
+	return (0);
+}
+
+int if_all_num(char *str)
+{
+	int	i;
+
+	if (str == NULL)
+		return (1);
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+long long	ft_stoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long long	result;
+
+	i = 0;
+	sign = 0;
+	result = 0;
+	while (str[i] == 32 || (str[i] <= 13 && str[i] >= 9))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = 1;
+		i++;
+	}
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result > (long)LONG_LONG_MAX + sign)
+			return (LONG_LONG_MAX);
+		i++;
+	}
+	if (sign)
+		result *= -1;
+	return (result);
+}
+
+int	builtin_exit(t_token *token)
+{
+	long long	nbr;
+
+	if (token == NULL)
+		return (1);
+	if (token->next == NULL)
+		exit(0);
+	else if (token->next != NULL)
+	{
+		token = token->next;
+		if (if_all_num(token->value) == 1)
+			return (1);
+		nbr = ft_stoi(token->value);
+		if (nbr == LONG_LONG_MAX)
+			return (1);
+	}
 }
