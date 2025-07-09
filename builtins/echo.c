b617eb8d2ebecabeb76d9d72ef0c3be5a6200492
Laghrabi <laghrabichaimae@gmail.com>
2025-07-09 18:37:52 +0100

    fixing norminette

--- builtins/echo.c
+++ builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zfarouk <zfarouk@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 22:47:08 by claghrab          #+#    #+#             */
-/*   Updated: 2025/07/08 21:58:04 by zfarouk          ###   ########.fr       */
+/*   Updated: 2025/07/09 17:08:43 by claghrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,34 +28,81 @@ int is_n_flag(char *flag)
 	return (0);
 }
 
-int	builtin_echo(t_token *token, t_env *env_list)
+int handle_echo_options(t_token **token, int *nl_flag)
 {
-	int	(i), (nl);
-	nl = 0;
-	if (token == NULL || env_list == NULL)
-		return (1);
-	token = token->next;
-	if (token != NULL && is_n_flag(token->value) == 0)
-	{
-		token = token->next;
-		nl = 1;
-	}
-	while (token != NULL)
-	{
-		i = 0;
-		while (token->value[i] != '\0')
-		{
-			if (write(1, &token->value[i], 1) == -1)
-					return (1);
-			i++;
-		}
-		if (token->next != NULL && (!token->ambiguous || token->empty))
-			write(1, " ", 1);
+    if (*token != NULL && is_n_flag((*token)->value) == 0)
+    {
+        *token = (*token)->next;
+        *nl_flag = 1;
+    }
+    return (0);
+}
+
+int print_echo_args(t_token *token, t_env *env_list, int nl_flag)
+{
+	int i;
+	
+    while (token != NULL)
+    {
+        i = 0;
+        while (token->value[i] != '\0')
+        {
+            if (write(1, &token->value[i], 1) == -1)
+                return 1;
+            i++;
+        }
+        if (token->next != NULL && (!token->ambiguous || token->empty))
+            write(1, " ", 1);
+
         if (token->next == NULL)
             update_env("_", token->value, env_list);
-		token = token->next;
-	}
-	if (nl == 0 && write(1, "\n", 1) == -1)
-		return (1);
-	return (0);
+        token = token->next;
+    }
+    if (nl_flag == 0 && write(1, "\n", 1) == -1)
+        return (1);
+    return (0);
 }
+
+int builtin_echo(t_token *token, t_env *env_list)
+{
+    int nl_flag;
+
+    if (token == NULL || env_list == NULL)
+		return 1;
+	nl_flag = 0;
+    token = token->next;
+    handle_echo_options(&token, &nl_flag);
+    return (print_echo_args(token, env_list, nl_flag));
+}
+
+// int	builtin_echo(t_token *token, t_env *env_list)
+// {
+// 	int	(i), (nl);
+// 	nl = 0;
+// 	if (token == NULL || env_list == NULL)
+// 		return (1);
+// 	token = token->next;
+// 	if (token != NULL && is_n_flag(token->value) == 0)
+// 	{
+// 		token = token->next;
+// 		nl = 1;
+// 	}
+// 	while (token != NULL)
+// 	{
+// 		i = 0;
+// 		while (token->value[i] != '\0')
+// 		{
+// 			if (write(1, &token->value[i], 1) == -1)
+// 					return (1);
+// 			i++;
+// 		}
+// 		if (token->next != NULL && (!token->ambiguous || token->empty))
+// 			write(1, " ", 1);
+//         if (token->next == NULL)
+//             update_env("_", token->value, env_list);
+// 		token = token->next;
+// 	}
+// 	if (nl == 0 && write(1, "\n", 1) == -1)
+// 		return (1);
+// 	return (0);
+// }
--- builtins/pwd.c
+++ builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zfarouk <zfarouk@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 22:48:27 by claghrab          #+#    #+#             */
-/*   Updated: 2025/07/09 11:18:49 by zfarouk          ###   ########.fr       */
+/*   Updated: 2025/07/09 16:52:25 by claghrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@ int	builtin_pwd(t_token *token, t_env *env_list)
 	
 	if (token == NULL || env_list == NULL)
 		return (1);
-	// if (token->next != NULL)
-	// {
-	// 	printf("pwd: too many arguments\n");
-	// 	return (1);
-	// }
 	pwd = get_env_value("PWD", env_list);
 	if (pwd == NULL)
 	{
